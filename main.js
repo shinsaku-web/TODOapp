d71dbac92b826a034f50674ca5e4104c1ad27d73
佐々木晨朔 <dawn.beginning.lope@gmail.com>
2022-01-27 09:59:53 +0900

    create機能の実装

--- main.js
+++ main.js
@@ -1,12 +1,31 @@
-const createBtn = document.getElementsByClassName('createToDo');
+const createBtn = document.getElementsByClassName('createToDoBtn');
 
-const formArea = document.getElementsByClassName('create');
+const createArea = document.getElementsByClassName('create');
 
-const bgArea = document.getElementsByClassName('bg');
+const form = document.getElementById('form');
 
+const bgElement = document.getElementsByClassName('bg');
 
-function clicked() {
-    bgArea.style = 'display:block';
-    formArea.style = 'display:block';
-    console.log('clicked!');
+function showCreateModal() {
+    bgElement[0].style.display = 'block';
+    createArea[0].style.display = 'block';
+    createBtn[0].style.display = 'none';
 }
+
+function hideCreateModal() {
+    bgElement[0].style.display = 'none';
+    createArea[0].style.display = 'none';
+    createBtn[0].style.display = 'block';
+}
+
+function submit(){
+    const inputVal = document.getElementsByName('todo');
+    const value = inputVal[0].value;
+    const todoList = document.getElementsByClassName('todos_list');
+    const todoItem = document.createElement("li");
+    todoItem.setAttribute('class','todos_item');
+    todoItem.innerText = value;
+    todoList[0].appendChild(todoItem);
+}
+
+form.addEventListener('submit',submit);
